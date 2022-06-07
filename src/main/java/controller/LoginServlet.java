3f5437b6e22afa618b8584a5ece0d2dbe7b04376
awesominki <oversteam@naver.com>
2022-06-07 14:11:07 +0900

    로그인 세션 수정

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class LoginServlet
@@ -35,18 +36,38 @@ public class LoginServlet extends HttpServlet {
 		MemberVO member = service.selectById(id, pass);
 		HttpSession session = request.getSession();
 		
+//		
+//		if (session.isNew()){
+//			if(id != null){
+//				session.setAttribute("id", id);
+//				String url=response.encodeURL("login");
+//			}else {
+//				session.invalidate();
+//			}
+//		}else{
+//			id = (String) session.getAttribute("user_id");
+//			if (id != null && id.length() != 0) {
+//			} else {
+//				session.invalidate();
+//			}
+//		}
+		
 		session.setAttribute("member", member);
-		String path = (String) session.getAttribute("reqPath");
+		//System.out.println(member.getId());
+//		String path = (String) session.getAttribute("member");
+		String path = null;
 		System.out.println("path=" + path);
 		
  		if (member == null) {
 			response.sendRedirect("login.do");
 		} else {
 			//--------------------------------------
-			if(path==null)
-				path = request.getContextPath() + "/index.jsp";
+			if(path==null) {
+				path = request.getContextPath();
+				response.sendRedirect("../main/main.jsp");
+			}
 			//--------------------------------------
-			response.sendRedirect(path);
+			//response.sendRedirect(path);
 		}
 	}
 
--- src/main/webapp/login/login.jsp
+++ src/main/webapp/login/login.jsp
@@ -4,10 +4,10 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>KO3 BANK</title>
 </head>
 <body>
-<h1>코삼뱅크</h1>
+<h1>KO3 BANK</h1>
 <form name="frmLogin" method="post" action="login.do" encType="UTF-8">
 	  	<input type="text" name="userid"><br>
        <input type="password" name="userpass"><br>
