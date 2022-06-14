c936f2af4e63b0295896566f5b0f89989d37d555
seeun <tpdms1945@gmail.com>
2022-06-14 22:47:23 +0900

    지금 커밋

---
+++ src/main/java/controller/NoticeUserDetailServlet.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.NoticeVO;
+import model.NoticeService;
+
+/**
+ * Servlet implementation class NoticeUserDetailServlet
+ */
+@WebServlet("/notice/noticeUserDetail.do")
+public class NoticeUserDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int num = Integer.parseInt(request.getParameter("noticeid"));
+		NoticeService service = new NoticeService();
+		
+		NoticeVO vo = service.selectById(num);
+		request.setAttribute("notice", vo);
+		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher("noticeUserDetail.jsp");
+		rd.forward(request, response);
+	}
+
+}
